6ae1d5f5198eb69bfee09cdbbfb429c732a0fd1a
Breno Ricco <brenoriccodev@gmail.com>
2019-04-25 10:52:51 -0300

    gonode gobarber

--- src/database/migrations/20190424210953-create-appointments.js
+++ src/database/migrations/20190424210953-create-appointments.js
@@ -20,6 +20,13 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE'
       },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE
